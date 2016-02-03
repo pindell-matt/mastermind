a2d588d7bf3962720e65eab61441a826efa266b4
Matt Pindell <pindell.m@gmail.com>
2016-02-02 20:38:31 -0700

    added response module and built rough working runner

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,56 @@
+require 'time'
 require 'pry'
+require_relative 'response'
 
 class Mastermind
+  include Response
+
+  attr_reader :start_time
+
+  def initialize
+    Response.welcome
+    loop do
+      eval_input(user_input.downcase)
+    end
+  end
+
+  def user_input
+    print "> "
+    gets.chomp
+  end
+
+  def eval_input(input)
+    if input == 'p' || input == 'play'
+      @start_time = Time.now
+      game_flow
+    elsif input == 'i' || input == 'instructions'
+      Response.instructions
+    elsif input == 'q' || input == 'quit'
+      abort("Thanks for playing!")
+    else
+      puts "Sorry - I don't understand!"
+    end
+  end
+
+  def game_flow
+    Response.start
+    loop do
+      guess = user_input.downcase
+      if guess == "test"
+        puts "CONGRATS"
+        puts @start_time
+        break
+      elsif guess == 'q' || guess == 'quit'
+        abort("Thanks for playing!")
+      end
+    end
+    Response.replay
+  end
 
 end
 
 if __FILE__ == $0
+
+Mastermind.new
 
 end
---
+++ response.rb
@@ -0,0 +1,24 @@
+module Response
+  # alt HASH or CONSTANT
+
+  def self.welcome
+    puts "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def self.instructions
+    puts "Try to guess the pattern, in both order and color, in the shortest number of turns as possible!"
+  end
+
+  def self.start
+    puts "I have generated a beginner sequence with four elements made up of: (r)ed,\n(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.\nWhat's your guess?"
+  end
+
+  def self.re_prompt
+    puts "Please guess a combination of (r)ed, (g)reen, (b)lue, or (y)ellow.\nUse (q)uit at any time to end the game."
+  end
+
+  def self.replay
+    puts "Do you want to (p)lay again or (q)uit?"
+  end
+
+end
